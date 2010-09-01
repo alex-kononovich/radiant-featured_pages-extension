bd56cf4a00dc041689130e2cbe816763d46faeba
Alex K <flskif@gmail.com>
2010-09-02 00:42:36 +0300

    Fixed migration: removed id column from habtm table

--- db/migrate/20100901200704_create_set_page_relationships.rb
+++ db/migrate/20100901200704_create_set_page_relationships.rb
@@ -1,6 +1,6 @@
 class CreateSetPageRelationships < ActiveRecord::Migration
   def self.up
-    create_table :featured_pages_sets_pages do |t|
+    create_table :featured_pages_sets_pages, { :id => false } do |t|
       t.references :featured_pages_set
       t.references :page
     end
